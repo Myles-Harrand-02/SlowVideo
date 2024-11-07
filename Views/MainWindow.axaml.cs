5fa7d8d96f2eb404b4c784e32cfd6cf54bd21023
Myles Harrand <harrandten@gmail.com>
2024-11-07 16:00:34 +0000

    Updating to new version using locally stored FFMpeg (Windows 10). Added comments to MainWindow cs.

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -18,28 +18,28 @@ namespace GetStartedApp.Views;
 
 public partial class MainWindow : Window
 {
-    private int _spd;
+    private int _spd; //video speed (unused)
 
     public int Spd;
 
-    private List<string> _locations = new List<string>();
+    private List<string> _locations = new List<string>(); //stores file locations of input files
 
-    private List<string> _names = new List<string>();
+    private List<string> _names = new List<string>(); //stores main name of input files
 
-    private string _output;
+    private string _output; //stores folder location for the output folder
 
     public MainWindow()
     {
         InitializeComponent();
 
-        fileCounter.Text = "0" + " Files";
+        fileCounter.Text = "0" + " Files"; //initialize information to display 0 files selected
 
         fileSize.Text = "0.0 MB";
     }
 
     private void ValChanged(object sender, NumericUpDownValueChangedEventArgs args)
     {
-        Trace.WriteLine("It Changed!");
+        //.WriteLine("It Changed!");
         Spd = Convert.ToInt32(Math.Round((decimal)NUP1.Value, 0));
     }
 
@@ -48,9 +48,9 @@ public partial class MainWindow : Window
         // Get top level from the current control
         var topLevel = TopLevel.GetTopLevel(this);
 
-        //This can also be applied for SaveFilePicker.
         var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
         {
+            //input file selector, allowing multiple video files
             Title = "Select Folder Containing Videos",
             AllowMultiple = true,
             FileTypeFilter = new[] { VideoAll }
@@ -58,6 +58,7 @@ public partial class MainWindow : Window
 
         fileCounter.Text = files.Count.ToString() + " Files";
 
+        //finds all the video file types selected and displays them to the user
         List<string> suffixes = new List<string>();
         ulong dataSize = 0;
 
@@ -67,9 +68,10 @@ public partial class MainWindow : Window
 
         foreach (var file in files)
         {
-
+            //collect file types from suffix
             var suff = file.Name.Substring(file.Name.Length - 4);
 
+            //only add to suffixes if the current suffix has not been used
             if (suffixes.Contains(suff))
             {
                 
@@ -79,6 +81,7 @@ public partial class MainWindow : Window
                 suffixes.Add(suff);
             }
 
+            //fetch file information to calculate size of selected videos
             var props = await file.GetBasicPropertiesAsync();
 
             if (props != null)
@@ -90,25 +93,29 @@ public partial class MainWindow : Window
 
             if (location != null)
             {
+                //adds the full location of each selected file
                 _locations.Add( (location.ToString()).Substring(8) );
 
+                //adds the main name (without suffix) of each selected file
                 _names.Add((file.Name).Remove(file.Name.Length - 4));
             }
         }
 
+        //displays the suffixes to the user with commas separating each
         fileTypes.Text = string.Join(", ", suffixes.ToArray());
 
+        //displays the file size in megabytes and rounds the final value
         fileSize.Text = ((float)dataSize / 1e6).ToString("F1") + " MB";
     }
 
     private async void btnOutput(object sender, RoutedEventArgs args)
     {
-        // Get top level from the current control
+
         var topLevel = TopLevel.GetTopLevel(this);
 
-        //This can also be applied for SaveFilePicker.
         var files = await topLevel.StorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions
         {
+            //output folder selector, allowing a single folder
             Title = "Select Folder Containing Videos",
             AllowMultiple = false,
         });
@@ -139,6 +146,7 @@ public partial class MainWindow : Window
             {
                 var mediaInfo = await FFProbe.AnalyseAsync(_locations[i]);
 
+                //old code using FFMpeg wrapper
                 /*FFMpegArguments
                     .FromFileInput(_locations[i])
                     .OutputToFile(_output + "/" + _names[i] + ".h264", false, options => options
@@ -152,7 +160,8 @@ public partial class MainWindow : Window
 
                 string outputLoc = _output + "/" + _names[i] + ".mp4";
 
-                string runCommand = string.Format("/C ffmpeg -i \"{0}\" -filter:v \"setpts={1}*PTS\" -an \"{2}\"", _locations[i], 2, outputLoc);
+                //new code running FFMpeg program stored locally. I use this method for speed
+                string runCommand = string.Format("/C ffmpeg -i \"{0}\" -threads 8 -r 24 -filter:v \"setpts={1}*PTS\" -an \"{2}\"", _locations[i], 2.5, outputLoc);
 
                 // run ffmpeg command line argument
                 Process process = new Process();
@@ -163,11 +172,14 @@ public partial class MainWindow : Window
 
                 process.StartInfo = startInfo;
                 process.Start();
+                process.WaitForExit();
 
                 completed++;
                 fileTime.Text = completed.ToString();//mediaInfo.Duration.TotalSeconds.ToString() + " ";
             }
         }
+
+        //checks the user has selected a valid input file and output folder
         else if (_locations.Count == 0)
         {
             fileWarn.Text = "Please select a video file to convert!";
@@ -177,10 +189,10 @@ public partial class MainWindow : Window
             fileWarn.Text = "Please select an output file!";
         }
 
-        fileTime.Text = "All Done!";
+        fileTime.Text = "All Done!"; //the task has finished
     }
 
-    
+    //old function for using FFMpeg wrapper to change video framerate
     private static bool newSpeedMP4(string input, string output, double fps, TimeSpan duration)
     {
         return FFMpegArguments
